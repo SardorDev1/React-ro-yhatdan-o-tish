935e526a6c85f2bb9affae32a06f88009ab3bf5a
SardorDev <sardordev1@mail.ru>
2022-11-02 14:11:59 +0500

    commit

--- Ro'yhatdan O'tish React/src/companent/Dashboard.jsx
+++ Ro'yhatdan O'tish React/src/companent/Dashboard.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import config from './firebase/config';
 import '../assets/style.css'
+// import server from './server/server'
+import { BrowserRouter , Router , Route } from 'react-router-dom';
 const Dashboard = () => {
     return (
         <div>
-         
+         <BrowserRouter>
+  <Router>       
+<Route path="/" component={} />
+<Route path="/all" element={Api_max} />
+</Router>
+
+
+</BrowserRouter>
             <button className='exit btn btn-danger' onClick={() => config.auth().signOut()}>Sign out</button>   
         </div>
     );
